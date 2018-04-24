7b19b7ce282e1d377b4b2c81be5154d18cbf08fa
Viclanda <Viclanda@DESKTOP-EV9FPLO>
2018-04-24 12:02:29 +0200

    d

--- gestion_incidencias/src/controlador/Principal.java
+++ gestion_incidencias/src/controlador/Principal.java
@@ -3,7 +3,7 @@ package controlador;
 import modelo.BBDD;
 import modelo.Usuario;
 
-public class principal {
+public class Principal {
 	public static void main(String[] args) {
 		UsuarioController uc = new UsuarioController();
 		Usuario u = new Usuario();
