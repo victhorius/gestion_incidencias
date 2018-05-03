e96f8281d37af03dde55509ce877f429736f1e07
Viclanda <Viclanda@DESKTOP-EV9FPLO>
2018-05-03 08:53:53 +0200

    *

--- gestion_incidencias/src/controlador/UsuarioController.java
+++ gestion_incidencias/src/controlador/UsuarioController.java
@@ -18,6 +18,7 @@ public class UsuarioController {
 		c.registrarUsuario(u);
 	}
 
+	//
 	public boolean Login(String user, String pw) {
 		boolean login = false;
 		Usuario u = new Usuario(user, pw);
