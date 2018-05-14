27cbfde307a225a7e35fcd4d10f4d0354d87fbd5
Miguel-97 <miguel97angel@gmail.com>
2018-05-14 09:19:40 +0200

    prueba

---
+++ gestion_incidencias/controlador/Principal.java
@@ -0,0 +1,18 @@
+package controlador;
+
+import vista.InicioSesion;
+
+public class Principal {
+	public static void main(String[] args) {
+		/*
+		 * UsuarioController uc = new UsuarioController(); IncidenciaController ic = new
+		 * IncidenciaController(); datosPantalla dp = new datosPantalla();
+		 * 
+		 * MenuController mc = new MenuController(dp); int opcion = dp.menuPrincipal();
+		 * mc.ejecucionMenu(opcion);
+		 */
+		InicioSesion log = new InicioSesion();
+		log.setVisible(true);
+
+	}
+}
